1127a24bd8dd6d4cb7bd5514f787b559fa4b9e15
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-16 17:50:14 +1300

    feat: invalid emails, username, and password static methods

--- src/core/errors/post.ts
+++ src/core/errors/post.ts
@@ -4,4 +4,36 @@ export class PostError extends Error {
   constructor() {
     super();
   }
+
+  static invalidUsername() {
+    return {
+      success: false,
+      details: {
+        reason:
+          "The username provided is not valid. Username can not contain spaces or special characters",
+        errorCode: 400,
+      },
+    };
+  }
+
+  static invalidEmail() {
+    return {
+      success: false,
+      details: {
+        reason: "The email provided is not valid",
+        errorCode: 400,
+      },
+    };
+  }
+
+  static invalidPassword() {
+    return {
+      success: false,
+      details: {
+        reason:
+          "The Password provided is not valid. It must contain at least one of the following letters: Uppercase, lowercase, special character, number. And at least eight characters",
+        errorCode: 400,
+      },
+    };
+  }
 }
