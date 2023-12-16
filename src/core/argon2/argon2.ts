a91674d4c565af855a6e66ae074a49aa70cf4ac3
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-16 19:16:13 +1300

    feat: argon2 hashing and verifiying password functions working

---
+++ src/core/argon2/argon2.ts
@@ -0,0 +1,21 @@
+import * as argon2 from "argon2";
+
+/**
+ * Async function to hash a password
+ * @param password
+ * @returns string
+ */
+async function hashPassword(password: string): Promise<string> {
+  return await argon2.hash(password, {
+    type: argon2.argon2id,
+  });
+}
+
+async function verifyPasswordHash(
+  hashPassword: string,
+  password: string
+): Promise<boolean> {
+  return await argon2.verify(hashPassword, password);
+}
+
+export { hashPassword, verifyPasswordHash };
