174cc51a9397977d83e890aba2440bdb385161ed
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-16 19:37:09 +1300

    feat: added new .env variables

--- src/core/types/environment.d.ts
+++ src/core/types/environment.d.ts
@@ -4,6 +4,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       ACCESS_TOKEN_SECRET: Secret | string;
+      EMAIL_USER: string;
+      EMAIL_PASSWORD: string;
     }
   }
 }
