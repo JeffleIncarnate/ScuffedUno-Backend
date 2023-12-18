e63eeb1f44d9c7c9579d953c2ef596967f18ad9c
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-18 17:37:33 +1300

    feat: added another base error static function

--- src/core/errors/error.ts
+++ src/core/errors/error.ts
@@ -1,10 +1,21 @@
 export class Error {
-  static didNotProideItems() {
+  static didNotProideItems(missing: string[]) {
     return {
       success: false,
       details: {
         reason: "An item was null or undefined, try proving all the items",
+        itemsMissing: missing,
         errorCode: 400,
+      },
+    };
+  }
+
+  static prismaError() {
+    return {
+      success: false,
+      details: {
+        reason: "Unknown database error",
+        errorCode: 500,
       },
     };
   }
