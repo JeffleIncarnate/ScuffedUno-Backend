050188c726695205d48c5461649947eff17fbf9d
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-18 17:38:47 +1300

    feat: added scopes to all the tokens

--- src/core/data/scopes.ts
+++ src/core/data/scopes.ts
@@ -1,4 +1,17 @@
-const scopes = {
+export interface IScopes {
+  socket: {
+    canCreateRoom: boolean;
+    canJoinRoom: boolean;
+    canPlayCard: boolean;
+  };
+  self: {
+    canDeleteSelf: boolean;
+    canDeleteOthers: boolean;
+    canCreateUSer: boolean;
+  };
+}
+
+const generalScopes: IScopes = {
   socket: {
     canCreateRoom: true,
     canJoinRoom: true,
@@ -7,7 +20,21 @@ const scopes = {
   self: {
     canDeleteSelf: true,
     canDeleteOthers: false,
+    canCreateUSer: false,
   },
 };
 
-export { scopes };
+const createUserScopes: IScopes = {
+  socket: {
+    canCreateRoom: false,
+    canJoinRoom: false,
+    canPlayCard: false,
+  },
+  self: {
+    canDeleteSelf: false,
+    canDeleteOthers: false,
+    canCreateUSer: true,
+  },
+};
+
+export { generalScopes, createUserScopes };
