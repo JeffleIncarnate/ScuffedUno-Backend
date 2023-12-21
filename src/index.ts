847d4f3db83e8610bb943959fed9eca601e3d91b
FusionSid <siddheshadsv@icloud.com>
2023-12-21 23:25:32 +1300

    feat: add route to exchange refresh tokens for access token

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,29 @@ import { Server } from "socket.io";
 
 import { logger } from "./core/logger/logger";
 
+// Import Routes
+import { login } from "./core/auth/authenticate";
+import { postUser } from "./routes/post/postUser";
+import { verifyUser } from "./routes/post/verifyUser";
+import { refreshTokenRoute } from "./core/auth/refresh";
+
 // Server
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
+   cors: {
+      origin: "*",
+   },
 });
 
 // Middleware
 app.use(morgan("dev"));
 app.use(express.json());
 
-// Import Routes
-// auth
-import { login } from "./core/auth/login";
-
-// post
-import { postUser } from "./routes/post/postUser";
-import { verifyUser } from "./routes/post/verifyUser";
-
 // Use Routes
 // auth
 app.use("/scuffed/auth/login", login);
+app.use("/scuffed/auth/refresh", refreshTokenRoute);
 
 // post
 app.use("/scuffed/post/postUser", postUser);
@@ -38,19 +37,19 @@ app.use("/scuffed/post/verifyUser", verifyUser);
 
 // Index Route
 app.all("/", (req, res) => {
-  return res.sendStatus(200);
+   return res.sendStatus(200);
 });
 
 app.all("*", (req, res) => {
-  return res
-    .status(404)
-    .send({ errorCode: 404, detail: "This route does not exist" });
+   return res
+      .status(404)
+      .send({ errorCode: 404, detail: "This route does not exist" });
 });
 
 io.on("connection", (socket) => {
-  console.log(socket);
+   console.log(socket);
 });
 
 httpServer.listen(3000, () => {
-  logger.info("API is running on port 3000");
+   logger.info("API is running on port 3000");
 });
