a2f1b11ced386701de1df8390c4cc05b6f0530bd
FusionSid <siddheshadsv@icloud.com>
2023-12-21 19:02:26 +1300

    fix: change status code from 400 -> 401 for login

--- src/core/errors/auth.ts
+++ src/core/errors/auth.ts
@@ -11,7 +11,7 @@ export class AuthError extends Error {
          details: {
             reason:
                "You do not exist in the database, so you are unable to get a token",
-            errorCode: 400,
+            errorCode: 401,
          },
       };
    }
@@ -21,7 +21,7 @@ export class AuthError extends Error {
          success: false,
          details: {
             reason: "The password you provided is not correct",
-            errorCode: 400,
+            errorCode: 401,
          },
       };
    }
@@ -32,7 +32,7 @@ export class AuthError extends Error {
          details: {
             reason:
                "You are not verified, so you are unable to get an access token. Read the instruction in your email to lean how to verify",
-            errorCode: 400,
+            errorCode: 401,
          },
       };
    }
