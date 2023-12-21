985236aa91838f3466a81fbdd212fee956450b36
FusionSid <siddheshadsv@icloud.com>
2023-12-21 18:48:58 +1300

    feat: add more specific errors for authentication middleware

--- src/core/auth/auth.ts
+++ src/core/auth/auth.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from "express";
 import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
 import { IScopes } from "../data/scopes";
 import { AuthError } from "../errors/auth";
 
@@ -10,31 +11,32 @@ interface IToken {
    exp: number;
 }
 
-function authToken(req: Request, res: Response, next: NextFunction) {
+export function authorizeRequest(
+   req: Request,
+   res: Response,
+   next: NextFunction,
+) {
    const auth_header = req.headers["authorization"];
    const token = auth_header && auth_header.split(" ")[1]; // Splitting because it goes: "Bearer [space] TOKEN"
 
-   if (token === null || token === undefined) return res.sendStatus(401);
-
-   try {
-      const user: IToken = jwt.verify(
-         token,
-         process.env.ACCESS_TOKEN_SECRET as string,
-      ) as IToken;
-
-      req.user = user;
-      next();
-   } catch (err) {
-      if (err instanceof JsonWebTokenError) {
-         return res
-            .status(AuthError.verifyTokenFail().details.errorCode)
-            .send(AuthError.verifyTokenFail());
-      } else {
-         return res
-            .status(AuthError.generalTokenFail().details.errorCode)
-            .send(AuthError.generalTokenFail());
-      }
+   if (token === undefined) {
+      const error = AuthError.tokenNotProvided();
+      return res.status(error.details.errorCode).send(error);
    }
-}
 
-export { authToken };
+   let user;
+   try {
+      user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+   } catch (err) {
+      if (!(err instanceof JsonWebTokenError)) {
+         const error = AuthError.generalTokenFail();
+         return res.status(error.details.errorCode).send(error);
+      }
+
+      const error = AuthError.invalidTokenProvided(err.message);
+      return res.status(error.details.errorCode).send(error);
+   }
+
+   req.user = user as IToken;
+   next();
+}
--- src/core/errors/auth.ts
+++ src/core/errors/auth.ts
@@ -1,59 +1,70 @@
 import { Error } from "./error";
 
 export class AuthError extends Error {
-  constructor() {
-    super();
-  }
+   constructor() {
+      super();
+   }
 
-  static youDoNotExist() {
-    return {
-      success: false,
-      details: {
-        reason:
-          "You do not exist in the database, so you are unable to get a token",
-        errorCode: 400,
-      },
-    };
-  }
+   static youDoNotExist() {
+      return {
+         success: false,
+         details: {
+            reason:
+               "You do not exist in the database, so you are unable to get a token",
+            errorCode: 400,
+         },
+      };
+   }
 
-  static incorrectPassword() {
-    return {
-      success: false,
-      details: {
-        reason: "The password you provided is not correct",
-        errorCode: 400,
-      },
-    };
-  }
+   static incorrectPassword() {
+      return {
+         success: false,
+         details: {
+            reason: "The password you provided is not correct",
+            errorCode: 400,
+         },
+      };
+   }
 
-  static notVerified() {
-    return {
-      success: false,
-      details: {
-        reason:
-          "You are not verified, so you are unable to get an access token. Read the instruction in your email to lean how to verify",
-        errorCode: 400,
-      },
-    };
-  }
+   static notVerified() {
+      return {
+         success: false,
+         details: {
+            reason:
+               "You are not verified, so you are unable to get an access token. Read the instruction in your email to lean how to verify",
+            errorCode: 400,
+         },
+      };
+   }
 
-  static verifyTokenFail() {
-    return {
-      success: false,
-      details: {
-        reason: "The token you are using is expired or is invalid",
-        errorCode: 400,
-      },
-    };
-  }
+   static invalidTokenProvided(reason: string) {
+      return {
+         success: false,
+         details: {
+            reason: "The token you provided is not valid",
+            error: reason,
+            errorCode: 401,
+         },
+      };
+   }
 
-  static generalTokenFail() {
-    return {
-      success: false,
-      details: {
-        reason: "There was an error with the token verification",
-        errorCode: 400,
-      },
-    };
-  }
+   static tokenNotProvided() {
+      return {
+         success: false,
+         details: {
+            reason: "You straight up forgor to provide a token",
+            errorCode: 400,
+         },
+      };
+   }
+
+   static generalTokenFail() {
+      return {
+         success: false,
+         details: {
+            reason: "There was an error with the token verification",
+            errorCode: 500,
+         },
+      };
+   }
 }
